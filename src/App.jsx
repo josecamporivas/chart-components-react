245443653b1a314d1af7451ef521e078d9d3c95d
Jose Campo Rivas <josecampor31@gmail.com>
2023-07-26 19:59:03 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      Hola buenas
+    </div>
+  )
+}
+
+export default App
