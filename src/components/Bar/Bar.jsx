1286c74ecab32166d96b6efee28e5a7c4a15fd14
Jose Campo Rivas <josecampor31@gmail.com>
2023-07-31 17:26:34 +0200

    add button return home

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -11,6 +11,7 @@ import {
   } from 'chart.js';
 import { useState } from 'react';
 import BarDataset from './BarDataset';
+import { Link } from 'react-router-dom';
   
 ChartJS.register(
   CategoryScale,
@@ -40,6 +41,7 @@ const DEFAULT_DATASETS = [
 export default function Bar(){
     const [valuesAxisX, setValuesAxisX] = useState(DEFAULT_VALUES_AXISX)
     const [datasets, setDatasets] = useState(DEFAULT_DATASETS)
+
     const data = {
         labels: valuesAxisX,
         datasets
@@ -65,28 +67,37 @@ export default function Bar(){
         id: crypto.randomUUID(),
         label: '',
         data: [0,0,0,0,0,0,0],
-        backgroundColor: '#' + Math.floor(Math.random()*16777215).toString(16)
+        backgroundColor: '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, Math.floor(Math.random()*9))
       }
       setDatasets([...datasets, emptyDataset])
     }
 
     return (
-      <main>
-        <section className='barContainer'>
-          <BarComponent data={data} className='bar'/>
-          {datasets.map(dataset => {
-            const {id, label, data, backgroundColor} = dataset
-            return <BarDataset key={id} id={id} label={label} data={data} 
-                    backgroundColor={backgroundColor} changeDataset={changeDataset}
-                    deleteDataset={deleteDataset} />
-          })} 
-        </section>
-        <section>
-          <button className='button' onClick={addDataset}>
-            <img className='icon' src='/addIcon.svg' alt='add icon'/>
+      <>
+        <nav className='navBar'>
+          <Link to='/' className='homeLink'>
+            <img className='icon' src='/arrowLeft.svg' alt='return home' />
+            <p>Home</p>
+          </Link>
           </button>
-        </section>
-      </main>
+        </nav>
+        <main>
+          <section className='barContainer'>
+            <BarComponent data={data} className='bar'/>
+            {datasets.map(dataset => {
+              const {id, label, data, backgroundColor} = dataset
+              return <BarDataset key={id} id={id} label={label} data={data} 
+              backgroundColor={backgroundColor} changeDataset={changeDataset}
+              deleteDataset={deleteDataset} />
+            })} 
+          </section>
+          <section>
+            <button className='button' onClick={addDataset}>
+              <img className='icon' src='/addIcon.svg' alt='add icon'/>
+            </button>
+          </section>
+        </main>
+      </>
     )
 }
 
